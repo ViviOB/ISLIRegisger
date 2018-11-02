629de19b9ae06dfb6deba12acbf80719bddee161
WTL123456 <18239558109@163.com>
2018-11-02 19:57:18 +0800

    Update 源代码管理/ISLIAPI/ISLI.Model/Videos.cs

---
+++ 源代码管理/ISLIAPI/ISLI.Model/Videos.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISLI.Model
+{
+    public class Videos
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string TypeID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string TypeName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Creator { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Desription { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Publisher { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PublisherPlace { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime ReleaseDate { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Identifier { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string LanguageId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CompressionRatio { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string RelatedResources { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string VideoLength { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Color { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FileSize { get; set; }
+    }
+}
