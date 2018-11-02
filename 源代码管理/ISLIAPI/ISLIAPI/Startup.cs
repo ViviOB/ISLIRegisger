be172f9c03d555b2f54e6e616b41b37f07ab87f1
ViviOB <44598261+ViviOB@users.noreply.github.com>
2018-11-02 10:56:50 +0800

    Update 源代码管理/ISLIAPI/ISLIAPI/Startup.cs

--- 源代码管理/ISLIAPI/ISLIAPI/Startup.cs
+++ 源代码管理/ISLIAPI/ISLIAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace ISLIAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connstr = "Server=localhost;Database=booksystem; User=root;Password=19950503;Use Procedure Bodies=false;Charset=utf8;Allow Zero Datetime=True; Pooling=false; Max Pool Size=50; ";
+            var connstr = "Server=169.254.35.148;Database=booksystem; User=root;Password=19950503;Use Procedure Bodies=false;Charset=utf8;Allow Zero Datetime=True; Pooling=false; Max Pool Size=50; ";
             BaseDB.DBConnectionString = Configuration.GetConnectionString(connstr);//初始化数据库连接字符串
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
