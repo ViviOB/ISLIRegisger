96e0c762d59b2c792b64ac7c088c710e4d6c57fd
WTL123456 <18239558109@163.com>
2018-11-06 11:30:12 +0800

    w

---
+++ 源代码管理/ISLIAPI/ISLIAPI/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ISLI.Model;
+using ISLI.IService;
+using ISLI.Service;
+namespace ISLIAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private readonly IBooks _books;
+        public BooksController(IBooks books)
+        {
+            _books = books;
+        }
+
+        /// <summary>
+        /// 显示所有
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Books> GetList()
+        {
+            var  list=  _books.BooksList();
+            return list;
+        }
+    }
+}
