511c41b8f7dc58dc4b842fcdabc5a16ee92de0ee
WTL123456 <18239558109@163.com>
2018-11-06 11:04:09 +0800

    w

--- 源代码管理/ISLIAPI/ISLI.Service/BooksService.cs
+++ 源代码管理/ISLIAPI/ISLI.Service/BooksService.cs
@@ -4,23 +4,55 @@ using System.Text;
 
 using ISLI.IService;
 using ISLI.Model;
+using SqlSugar;
+using ISLI.Unility;
+
 namespace ISLI.Service
 {
     public class BooksService : IBooks
     {
+        /// <summary>
+        /// 添加图书，ISLI编码
+        /// </summary>
+        /// <param name="books"></param>
+        /// <returns></returns>
         public int Add(Books books)
         {
-            throw new NotImplementedException();
+            using (SqlSugarClient db =BaseDB.GetClient())
+            {
+                //插入并返回受影响行数用ExecuteCommand 
+                int i = db.Insertable<Books>(books).ExecuteCommand();
+                return i;
+            }
         }
 
+        /// <summary>
+        /// 显示所有
+        /// </summary>
+        /// <returns></returns>
         public List<Books> BooksList()
         {
-            throw new NotImplementedException();
+            using (SqlSugarClient db = BaseDB.GetClient())
+            {
+                var getList = db.Queryable<Books>().ToList();
+                return getList;
+
+            }
         }
+
+        /// <summary>
+        /// 修改图书,ISLI编码
+        /// </summary>
+        /// <param name="books"></param>
+        /// <returns></returns>
 
         public int Update(Books books)
         {
-            throw new NotImplementedException();
+            using (SqlSugarClient db=BaseDB.GetClient())
+            {
+                var i = db.Updateable(books).ExecuteCommand();
+                return i;
+            }
         }
     }
 }
