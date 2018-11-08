7c79c330f2318464c10e60b1e2659f2f9392e2a5
Hikarurt <Hikarurt@outlook.com>
2018-11-08 13:25:30 +0800

    Update 源代码管理/ISLIAPI/ISLIAPI/Controllers/BackStageController.cs

--- 源代码管理/ISLIAPI/ISLIAPI/Controllers/BackStageController.cs
+++ 源代码管理/ISLIAPI/ISLIAPI/Controllers/BackStageController.cs
@@ -12,15 +12,43 @@ namespace ISLIAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthorizeController : ControllerBase
+    public class BackStageController : ControllerBase
     {
         /// <summary>
         /// 构造函数注入
         /// </summary>
-        private readonly IAuthorize _authorize;
-        public AuthorizeController(IAuthorize authorize)
+        private readonly IPublishApply _publishApply;
+        public BackStageController(IPublishApply publishApply)
         {
-            _authorize = authorize;
+            _publishApply = publishApply;
         }
+
+        #region ///出版单位管理
+        /// <summary>
+        /// 获取获取出版单位申请
+        /// </summary>
+        /// <returns></returns>
+        public List<PublishApply> GetPublishApplysList()
+        {
+            return _publishApply.GetPublishApplysList();
+        }
+
+        /// <summary>
+        /// 根据ID获取对象
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Publisher UpdateById(int id)
+        {
+            return _publishApply.UpdateById(id);
+        }
+        #endregion
+
+        #region ///服务商管理
+
+        #endregion
+
+        #region ///ISLI编码申请管理
+        #endregion
     }
 }
