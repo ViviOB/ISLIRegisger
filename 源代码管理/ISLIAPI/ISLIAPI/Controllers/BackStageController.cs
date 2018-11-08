af2f9d6f8e4b18eff9311126df464887d26346e3
Hikarurt <Hikarurt@outlook.com>
2018-11-08 13:16:44 +0800

    ss

---
+++ 源代码管理/ISLIAPI/ISLIAPI/Controllers/BackStageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ISLI.IService;
+using ISLI.Model;
+
+namespace ISLIAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizeController : ControllerBase
+    {
+        /// <summary>
+        /// 构造函数注入
+        /// </summary>
+        private readonly IAuthorize _authorize;
+        public AuthorizeController(IAuthorize authorize)
+        {
+            _authorize = authorize;
+        }
+    }
+}
