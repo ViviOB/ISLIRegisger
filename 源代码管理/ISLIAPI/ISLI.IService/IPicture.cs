7fb72687ca41e7daad32e3098ae4c34716e8b9ab
WTL123456 <18239558109@163.com>
2018-11-08 15:37:32 +0800

    Revert "Revert"
    This reverts commit 591b7c7411c573f72b8bed72b891aa6aa65e812e.

---
+++ 源代码管理/ISLIAPI/ISLI.IService/IPicture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ISLI.Model;
+namespace ISLI.IService
+{
+    public interface IPicture
+    {
+        /// <summary>
+        /// 添加图片信息
+        /// </summary>
+        /// <param name="audio"></param>
+        /// <returns></returns>
+        int AddPicture(Picture audio);
+
+        /// <summary>
+        /// 显示图片信息
+        /// </summary>
+        /// <returns></returns>
+        List<Picture> GetPicture();
+    }
+}
