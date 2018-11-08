ee6f059b86db71cc4910fc255a76aed6252b1fe4
WTL123456 <18239558109@163.com>
2018-11-08 19:02:41 +0800

    1

---
+++ 源代码管理/ISLIAPI/ISLIAPI/Controllers/RichTextController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ISLI.IService;
+using ISLI.Service;
+using ISLI.Model;
+using Microsoft.AspNetCore.Cors;
+
+namespace ISLIAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("CorsConfig")]
+    [ApiController]
+    public class RichTextController : ControllerBase
+    {
+        private readonly IRichText _richtext;
+        public RichTextController(IRichText richText)
+        {
+            _richtext = richText;
+        }
+
+        /// <summary>
+        /// 显示富文本
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<RichText>GetRichTexts()
+        {
+            var list = _richtext.GetRichTexts();
+            return list;
+        }
+
+        /// <summary>
+        /// 添加富文本
+        /// </summary>
+        /// <param name="richText"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public int AddRichText(RichText richText)
+        {
+            var i = _richtext.AddRichText(richText);
+            return i;
+        }
+    }
+}
