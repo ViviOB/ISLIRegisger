83b813b8ee02a9d044c40ed0f8dd2a980b090f01
ViviOB <44598261+ViviOB@users.noreply.github.com>
2018-11-13 19:11:58 +0800

    Update 源代码管理/ISLIClient/ISLIClient/Filter/PermissionFilter.cs

--- 源代码管理/ISLIClient/ISLIClient/Filter/PermissionFilter.cs
+++ 源代码管理/ISLIClient/ISLIClient/Filter/PermissionFilter.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ISLIClient.Filter
 {
-    public class PermissionFilter
+    public class PermissionFilter : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //获取访问路径
+            var path = filterContext.HttpContext.Request.Path.ToString();
+
+            var user = filterContext.HttpContext.User;
+            if (user.Claims.Count() > 0)
+            {
+                var result = user.Claims.Where(m => m.Type == "keykey").First().Value;
+                if (result != "qqq")
+                {
+                    filterContext.Result = new RedirectResult("/Account/Index");
+                    base.OnActionExecuting(filterContext);
+                    return;
+                }
+            }
+            filterContext.Result = new RedirectResult("/Account/Index");
+            base.OnActionExecuting(filterContext);
+        }
     }
 }
